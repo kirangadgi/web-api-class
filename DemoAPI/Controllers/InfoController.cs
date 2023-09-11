bbb4d038eff7d293a76f0f415636ab1f5d55873e
kiran <kiran_kumar_4@progressive.com>
2023-09-11 12:27:41 -0400

    Demo App build before lunch monday

---
+++ DemoAPI/Controllers/InfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers;
+
+public class InfoController : ControllerBase
+{
+    [HttpGet("/info")]
+    public async Task<ActionResult> GetTheInfo()
+    {
+        return Ok("The Controller is working, thanks for trying");
+    }
+}
