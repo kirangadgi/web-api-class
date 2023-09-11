f1dd6b60d95a137940b6fb5a45d0fa1a7d3abc00
kiran <kiran_kumar_4@progressive.com>
2023-09-11 14:33:49 -0400

    Update after lunch

--- DemoAPI/Controllers/InfoController.cs
+++ DemoAPI/Controllers/InfoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 
 namespace DemoAPI.Controllers;
 
@@ -9,4 +10,61 @@ public class InfoController : ControllerBase
     {
         return Ok("The Controller is working, thanks for trying");
     }
+
+    [HttpGet("/blog/{year:int:min(2005)}/{month:int:min(1):max(12)}/{day:int:min(1):max(31)}")]
+    public async Task<ActionResult> Blog(int year, int month, int day)
+    {
+        return Ok($"Got it {year} {month} {day}");
+    }
+
+    //Get /colors/{color}
+
+    [HttpGet("/colors")]
+    public async Task<ActionResult> GetColors([FromQuery] string color = "Blue")
+    {
+        return Ok($"Color is {color}");
+    }
+
+    [HttpGet("/employees")]
+
+    public async Task<ActionResult> GetEmployees([FromQuery] string department = "All")
+
+    {
+
+        var employees = new List<Employee>
+
+        {
+
+            new Employee("Bob Smith", "dev"),
+
+            new Employee("Joe Jones", "dev"),
+
+            new Employee("Sue Blue", "ceo")
+
+        };
+
+        if (department != "All")
+
+        {
+
+            var response = employees.Where(e => e.Department == department).ToList();
+
+            return Ok(new ResponseType<List<Employee>>(response, department));
+
+        }
+
+        return Ok(new ResponseType<List<Employee>>(employees, department));
+
+    }
+    [HttpGet("/WhoAmI")]
+    public async Task<ActionResult> WhoAmi([FromHeader(Name ="User-Agent")]string userAgent)
+    {
+        return Ok($"You are running {userAgent}");
+
+    }
+
+
+    public record ResponseType<T>(T data, string Filter);
+
+    public record Employee(string Name, string Department);
 }
